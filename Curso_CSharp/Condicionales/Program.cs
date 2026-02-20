1fd34536e4c4036ab3dfd0fd8d7751fd3b257d97
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2026-02-19 23:12:33 -0500

    Introducción  condicional IF - ELSE IF - ELSE

---
+++ Curso_CSharp/Condicionales/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 10;
+
+            if (valor > 10)
+            {
+                Console.WriteLine("El valor es mayor a 10");
+            }
+            else if (valor == 10)
+            {
+                Console.WriteLine("El valor es igual a 10");
+            }
+            else
+            {
+                Console.WriteLine("El valor es menor a 10");
+            }
+        }
+    }
+}
