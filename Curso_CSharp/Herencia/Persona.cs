f90d1c0da03c09c5df7cb5a1e7b4ce6e40cb9fc8
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2026-03-19 22:42:27 -0500

    Creacion clase Persona

--- Curso_CSharp/Herencia/Persona.cs
+++ Curso_CSharp/Herencia/Persona.cs
@@ -6,5 +6,32 @@ namespace Herencia
 {
     internal class Persona
     {
+        // Atributos
+        private string nombres;
+        private string apellidos;
+        private string numId;
+        private string direccion;
+        private string telefono;
+
+        // Constructor
+        public Persona(string nombres, string apellidos, string numId, string direccion, string telefono)
+        {
+            this.nombres = nombres;
+            this.apellidos = apellidos;
+            this.numId = numId;
+            this.direccion = direccion;
+            this.telefono = telefono;
+        }
+
+        // Métodos GET
+        public string getNombres() {  return nombres; }
+
+        public string getApellidos() { return apellidos; }
+
+        public string getNumId() { return numId; }
+
+        public string getDireccion() { return direccion; }
+
+        public string getTelefono() { return telefono; }
     }
 }
