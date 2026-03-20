d54c296788cc0178e1c7d71af4081cd0055ac0bd
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2026-03-19 22:50:51 -0500

    Se creo  la clase CLientes heredando de su clase padre Persona

---
+++ Curso_CSharp/Herencia/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herencia
+{
+    internal class Cliente : Persona
+    {
+        // Atributos
+        private int puntos;
+
+        // Constructor
+        public Cliente(string nombres, string apellidos, string numId, string direccion, string telefono, int puntos) : base(nombres, apellidos, numId, direccion, telefono)
+        {
+            this.puntos = puntos;
+        }
+
+        // Metodo GET y SET
+        public string getPuntos() { return puntos.ToString(); }
+
+        public void setPuntos(int puntos) { this.puntos = puntos; }
+    }
+}
