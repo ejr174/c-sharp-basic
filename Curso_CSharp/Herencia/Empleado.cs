e2593810936c6ba8dcbfc5dd9a00acab84e495b9
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2026-03-19 22:52:37 -0500

    Se agrego la clase Empleado heredando de la cls padre Perosna

---
+++ Curso_CSharp/Herencia/Empleado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herencia
+{
+    internal class Empleado : Persona
+    {
+        // Atributos
+        private double salario;
+
+        // Constructor
+
+        public Empleado(string nombres, string apellidos, string numId, string direccion, string telefono, double salario) : base(nombres, apellidos, numId, direccion, telefono)
+        {
+            this.salario = salario;
+        }
+
+        // Metodo GET y SET
+        public string getSalario() { return salario.ToString(); }
+
+        public void setSalario(double salario) { this.salario = salario; }
+    }
+}
