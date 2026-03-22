2e357af2a5bc09c682d2d0abf3fe35f30b4b52c8
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2026-03-22 11:20:19 -0500

    Intro: Super Clase Object  y Casting que sirve para poder cambiar o transformar el tipo de datos de una variable a otra siempre y cuando haya una relacion entre ellas

--- Curso_CSharp/Herencia/Form1.cs
+++ Curso_CSharp/Herencia/Form1.cs
@@ -12,11 +12,21 @@ namespace Herencia
             Cliente client1 = new Cliente("Emmanuel", "Jaramillo Restrepo", "34234", "cll 50 # 35-24", "2345434", 50);
             Empleado empleado1 = new Empleado("Juan", "Perez", "12345", "carrea falsa # 29-31", "3004355", 20000);
 
-           // MessageBox.Show("El cliente " + client1.getNombres() + " tiene " + client1.getPuntos() + " puntos.");
-           //MessageBox.Show("El empleado " + empleado1.getNombres() + " tiene un salario de " + empleado1.getSalario() + ".");
+            // MessageBox.Show("El cliente " + client1.getNombres() + " tiene " + client1.getPuntos() + " puntos.");
+            //MessageBox.Show("El empleado " + empleado1.getNombres() + " tiene un salario de " + empleado1.getSalario() + ".");
 
-            client1.getInfo();
-            empleado1.getInfo(); 
+            //client1.getInfo();
+            //empleado1.getInfo(); 
+
+            // Ejemplo super clase OBJECT  y Casting
+            Object salome = new Cliente("Salome", "Medina Patino", "954065", "cll 50 # 35-24", "2345434", 50);
+
+            // Casting
+            Cliente clienteSalome = (Cliente) salome;
+
+            clienteSalome.getInfo();
+
+
         }
     }
 }
