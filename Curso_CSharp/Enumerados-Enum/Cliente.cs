b02a0f966dbc6151ffdeb709017979c2ce8bcd20
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2026-03-23 14:43:37 -0500

    Intro: Tipos enumerados: Estas definen un conjunto de constante NOMBRADAS, generalmente asociadas a un numero entero

---
+++ Curso_CSharp/Enumerados-Enum/Cliente.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enumerados_Enum
+{
+    internal class Cliente
+    {
+        // Atributos
+        private string nombres;
+        private string apellidos;
+        private string numId;
+        private string celular;
+        private string tipoCliente;
+
+        // Constructor
+        public Cliente(string nombres, string apellidos, string numId, string celular, TipoCliente tc)
+        {
+            this.nombres = nombres;
+            this.apellidos = apellidos;
+            this.numId = numId;
+            this.celular = celular;
+            this.tipoCliente = tc.ToString();
+
+        }
+
+        public Cliente() { }
+
+        // Métodos Setters y Getters
+        public string NOMBRES
+        {
+            get { return nombres; }
+            set { nombres = value; }
+        }
+
+        public string APELLIDOS
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+        public string NUMID
+        {
+            get { return numId; }
+            set { numId = value; }
+        }
+
+        public string CELULAR
+        {
+            get { return celular; }
+            set { celular = value; }
+        }
+
+        public string TIPODECLIENTE
+        {
+            get { return tipoCliente; }
+            set { tipoCliente = value; }
+        }
+
+        public enum TipoCliente
+        {
+            Regular = 100,
+            Premium = 500,
+            VIP = 1000
+        }
+
+    }
+}
---
+++ Curso_CSharp/Enumerados-Enum/Form1.cs
@@ -0,0 +1,20 @@
+using static Enumerados_Enum.Cliente;
+
+namespace Enumerados_Enum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cliente cliente1 = new Cliente("Juan", "Pérez", "12345678", "987654321", TipoCliente.Premium);
+
+            MessageBox.Show(cliente1.TIPODECLIENTE.ToString());
+
+        }
+    }
+}
