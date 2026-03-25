43d73203825ccbcadc4a204efabc0630a388263a
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2026-03-24 22:27:08 -0500

    Intro a las clases que son como un molde y los objetos que son las intancias de la clases.

---
+++ Curso_Backend_CSharp/Curso-Backend/IntroCsharpBackend/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+var persona1 = new Persona("Emmanuel");
+
+persona1.Nombre = "Cristiano";
+
+
+var message = persona1.Saludar();
+
+Console.WriteLine(message);
+
+
+class Persona 
+{
+    public string Nombre { get; set; }
+    private int Edad { get; set; }
+
+    public Persona(string nombre) {
+        this.Nombre = nombre;
+        Edad = 0;
+    }
+
+    public string Saludar() {
+        return "Hola, " + this.Nombre + "!";
+    }
+}
