f7a9bd97cf731d4eb4bdcc76f440e66f08393707
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2026-03-29 13:23:24 -0500

    Intro a la seriaizacion y deserialización de objetos. Serializar es el proceso por el podemos convertir un objeto en un cadena de texto que puede ser almacena o transmitida y deserializacion es el proceso por el cual convertimos la cadena de texto generada nuevamente en un objeto

--- Curso_Backend_CSharp/Curso-Backend/IntroCsharpBackend/Program.cs
+++ Curso_Backend_CSharp/Curso-Backend/IntroCsharpBackend/Program.cs
@@ -1,49 +1,26 @@
 ﻿
-// Genericos: Podemos utilizar genéricos para crear clases o métodos
-// que puedan trabajar con diferentes tipos de datos sin necesidad de duplicar código.
+// Serializacion y Deserializacion de objetos en C#
+// Para serializar un objeto, se puede utilizar la clase JsonSerializer del espacio de nombres System.Text.Json
 
-var personajes = new MyList<string>(3);
-var numeros = new MyList<int>(3);
+// La serialización es el proceso de convertir un objeto en una cadena de texto que puede ser almacenada o transmitida.
+// La deserialización es el proceso inverso, donde se convierte una cadena de texto en un objeto.
 
-personajes.AddElement("Mario");
-personajes.AddElement("Luigi");
-personajes.AddElement("Peach");
-//personajes.AddElement("Bowser"); // No se puede agregar, límite alcanzado
+using System.Text.Json;
 
-Console.WriteLine(personajes.GetContent());
+var emmanuel = new People { Name = "Emmanuel", age = 30 };
 
-public class MyList<T>
+// Serializar el objeto a una cadena JSON
+string jsonString = JsonSerializer.Serialize(emmanuel);
+Console.WriteLine("Objeto serializado a JSON: " + jsonString);
+
+// Deserializar la cadena JSON de vuelta a un objeto
+People emmanuelj = JsonSerializer.Deserialize<People>(jsonString);
+Console.WriteLine("Objeto deserializado: Nombre = " + emmanuelj.Name + " - Edad = " + emmanuelj.age);
+
+
+
+public class People
 {
-    private List<T> _list;
-    private int _limit;
-
-    public MyList(int limit)
-    {
-        _limit = limit;
-        _list = new List<T>();
-    }
-
-    public void AddElement(T element)
-    {
-        if (_list.Count < _limit)
-        {
-            _list.Add(element);
-        }
-        else
-        {
-            Console.WriteLine("No se pueden agregar más elementos, límite alcanzado.");
-        }
-    }
-
-    public string GetContent()
-    {
-        string content = "";
-
-        foreach (var element in _list)
-        {
-            content += element + ", ";
-        }
-
-        return content;
-    }
+    public string Name { get; set; }
+    public int age { get; set; }
 }
